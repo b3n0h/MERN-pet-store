2b3e666711a66e580325aed8db9a7a3694e12cdb
b3n0h <b3njaminoh@gmail.com>
2023-10-16 11:21:20 -0700

    successfully displaying data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    try {
+      axios
+        .get('http://localhost:5555/pets')
+        .then((res) => {
+          setPets(res.data.data)
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
   return (
     <div>
-      hello
+      <ul>
+        {pets.map((pet) => {
+          return <li>{pet.name}, {pet.breed}</li>
+        })}
+      </ul>
     </div>
   );
 }
